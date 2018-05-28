3656afc190499ac219cc2355f894545e33aa8600
Alexander "hippelalex" Yarmola <hippelalex@gmail.com>
2018-05-28 22:25:18 +0300

    update main.js

--- js/main.js
+++ js/main.js
@@ -7,22 +7,30 @@ function toggleMenu(){
 menu.classList.toggle('opened');
 }
 /**********************PROGRESSBAR***********************/
-var progressbar = document.getElementsByClassName('progressbar')[0];
+var progressbar = document.getElementsByClassName('progressbar');
+// progressbar.forEach(function(element) {
+//     console.log(0);
+// });
+
 var progress = document.getElementsByClassName('progress')[0];
+// console.log(progress)
+// var span = document.getElementsByTagName('span')[0];
 var currentprogress = 0;
 var value = progressbar.getAttribute('data-value');
 var interval = setInterval(function(){
- animateProgress();
+    animateProgress();
 }, 20);
 function animateProgress(){
 
-	if(currentprogress <= value){
-	 progress.style.width = currentprogress + '%';
-	 currentprogress++;
-	}
-	else{
-	 clearInterval(interval);
-	}
+    if(currentprogress <= value){
+        progress.style.width = currentprogress + '%';
+        // span.innerText = currentprogress + '%';
+        currentprogress++;
+    }
+    else{
+        clearInterval(interval);
+    }
 
 }
+
 
